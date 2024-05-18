4519e5a2aed543747c32b1c5643a842b575772f8
Black Lover <158266786+BLACKLOVER15@users.noreply.github.com>
2024-05-18 17:38:08 +0530

    Update config.py
    Signed-off-by: Black Lover  <158266786+BLACKLOVER15@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/BlackMusicSupport",
+    "https://github.com/BLACKLOVER15/BlackXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
