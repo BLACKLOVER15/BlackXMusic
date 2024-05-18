458fe0543175146e0fa6c22edab543d340b37d4c
Black Lover <158266786+BLACKLOVER15@users.noreply.github.com>
2024-05-18 17:41:54 +0530

    Update userbot.py
    Signed-off-by: Black Lover  <158266786+BLACKLOVER15@users.noreply.github.com>

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,7 +51,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Exampur20")
+                await self.one.join_chat("MDCLASSES_OFFICIAL")
                 await self.one.join_chat("BlackMusicSupport")
             except:
                 pass
@@ -72,7 +72,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Exampur20")
+                await self.two.join_chat("MDCLASSES_OFFICIAL")
                 await self.two.join_chat("BlackMusicSupport")
             except:
                 pass
@@ -93,7 +93,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Exampur20")
+                await self.three.join_chat("MDCLASSES_OFFICIAL")
                 await self.three.join_chat("BlackMusicSupport")
             except:
                 pass
@@ -114,7 +114,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Exampur20")
+                await self.four.join_chat("MDCLASSES_OFFICIAL")
                 await self.four.join_chat("BlackMusicSupport")
             except:
                 pass
@@ -135,7 +135,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Exampur20")
+                await self.five.join_chat("MDCLASSES_OFFICIAL")
                 await self.five.join_chat("BlackMusicSupport")
             except:
                 pass
